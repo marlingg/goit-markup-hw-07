33e493fd1dbc3e21744f03c35496f1aa3071780a
marlingg <123587599+marlingg@users.noreply.github.com>
2023-05-07 14:04:03 +0300

    qwe

--- js/modal.js
+++ js/modal.js
@@ -18,9 +18,19 @@ function header_main(){
   let win = document.getElementById('header_window');
  
   let style = getComputedStyle(win);
-      if (style.display !== 'block') {
+      if (style.display === 'none') {
           document.getElementById('header_window').style.display= 'block';
-  }else if (style.display=== 'block') {
+  }else if (style.display === 'block') {
           document.getElementById('header_window').style.display= 'none';
+  }
+}
+function win_exit() { 
+  
+  let win = document.getElementById('header_window');
+ 
+  let style = getComputedStyle(win);
+      if (style.display === 'block') {
+          document.getElementById('header_window').style.display= 'none';
+ 
   }
 }
